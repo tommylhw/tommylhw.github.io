808bfea54e04f9bf175f9288ec76c20255bf9fcf
tommylhw <62752366+tommylhw@users.noreply.github.com>
2021-12-13 12:05:04 +0800

    1
    1

--- js/SlideController.js
+++ js/SlideController.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > 5) {
+		if ($(this).scrollTop() > 10) {
+			$('header').css("box-shadow", "0 0 30px grey");
+			$('header').css("padding", "0 20px");
 			$('header').css("background", "var(--purple-theme-color)");
 			$('header a').css("color", "#fff");
 			$('header ul li').hover(function() {
@@ -9,7 +11,10 @@ $(document).ready(function () {
 			$('header ul li').mouseleave(function() {
 				$(this).css("border", "1px solid rgba(33,33,33,0)");
 			});
+			$('.nav-icon3-container').css("top", "23px");
 		} else {
+			$('header').css("box-shadow", "none");
+			$('header').css("padding", "20px 20px");
 			$('header').css("background", "rgba(33,33,33,0)");
 			$('header a').css("color", "var(--purple-theme-color)");
 			$('header ul li').hover(function() {
@@ -18,6 +23,7 @@ $(document).ready(function () {
 			$('header ul li').mouseleave(function() {
 				$(this).css("border", "1px solid rgba(33,33,33,0)");
 			});
+			$('.nav-icon3-container').css("top", "40px");
 		}
 	});
 });
