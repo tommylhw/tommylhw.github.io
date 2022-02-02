b33b168dc89761a048766bb289b4ba49ea59e7a2
tommylhw <62752366+tommylhw@users.noreply.github.com>
2022-02-02 17:27:39 +0800

    1

--- js/tabbed-content.js
+++ js/tabbed-content.js
@@ -1,15 +1,19 @@
-window.onload = (event) => {
-  OpenSection('About');
-};
+// window.onload = (event) => {
+//   OpenSection('Awards');
+//   // document.getElementById('AboutButton').style.background = "var(--light-blue);";
+// };
 
-function OpenSection(topicName) {
-  var i;
-  var topicID = document.getElementsByClassName('topic-section');
+// function OpenSection(topicName) {
+//   var i;
+//   var topicID = document.getElementsByClassName('topic-section');
+//   var btnID = document.getElementsByClassName('menu-btn');
 
-  for (i = 0; i < topicID.length; i++) {
-    topicID[i].style.display = "none";
-  }
+//   for (i = 0; i < topicID.length; i++) {
+//     topicID[i].style.display = "none";
+//     // btnID[i].style.background = "var(--dark-blue)";
+//   }
 
-  document.getElementById(topicName).style.display = "block";
+//   document.getElementById(topicName).style.display = "block";
+//   // document.getElementById(topicName + "Button").style.display = "var(--light-blue);"
 
-}
+// }
