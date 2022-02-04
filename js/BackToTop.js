d22e78ff146eba900553c86af307acc1999f368c
tommylhw <62752366+tommylhw@users.noreply.github.com>
2022-02-04 18:04:03 +0800

    1

--- js/BackToTop.js
+++ js/BackToTop.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 100) {
+			// $('.scroll-top').css('display', 'block');
 			$('.scroll-top').fadeIn();
 		} else {
 			$('.scroll-top').fadeOut();
+			// $('.scroll-top').css('display', 'none');
 		}
 	});
 
