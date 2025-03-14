03c8dd530de53ba5e70f080dcd83e4a84cfdb9f8
tommylhw <62752366+tommylhw@users.noreply.github.com>
2025-03-15 02:37:08 +0800

    next

--- src/components/reactbit/TextAnimations/TrueFocus/TrueFocus.tsx
+++ src/components/reactbit/TextAnimations/TrueFocus/TrueFocus.tsx
@@ -99,7 +99,7 @@ const TrueFocus: React.FC<TrueFocusProps> = ({
         return (
           <span
             key={index}
-            ref={(el) => (wordRefs.current[index] = el)}
+            // ref={(el) => (wordRefs.current[index] = el)}
             // className="relative text-[3rem] font-black cursor-pointer"
             style={{
               ...textStyles, // Spread the base textStyles object
