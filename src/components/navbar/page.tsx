6a876fe5840278f442fac073aeb82be3cca8d758
tommylhw <62752366+tommylhw@users.noreply.github.com>
2025-03-16 21:34:59 +0800

    fixed navbar for production

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import style from "./navbar.module.css";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
