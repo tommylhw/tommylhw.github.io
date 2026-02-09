b146d90c29ef4c74bddc87d94368b3eeb8dcfb3d
tommylhw <62752366+tommylhw@users.noreply.github.com>
2026-02-09 21:56:49 +0800

    hide awards section to test performance

--- src/components/recognition/recognition.tsx
+++ src/components/recognition/recognition.tsx
@@ -59,7 +59,7 @@ const Recognition = () => {
       className="max-w-[1200px] w-full flex flex-col justify-center item-center" /* className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-40" */
     >
       {/* <Tabs tabs={tabs} /> */}
-      <Awards />
+      {/* <Awards /> */}
       <Media />
     </div>
   );
